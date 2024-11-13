9068b5e04823ffcb0dba17e7b432d555c0a9478a
kempix360 <108021275+kempix360@users.noreply.github.com>
2024-11-13 23:44:59 +0100

    Add files via upload

--- usbhid_module.c
+++ usbhid_module.c
@@ -1,30 +1,37 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/input.h>
+#include <linux/input.h> // defines standard keycodes
 #include <linux/hid.h>
 #include <linux/keyboard.h>
 #include <linux/uaccess.h>
 
 static struct timer_list hello_timer;
 
-static void hello_timer_fn(struct timer_list *t) {
-    pr_info("Hello\n");
-}
+// static void hello_timer_fn(struct timer_list *t) {
+//     pr_info("Hello\n");
+// }
 
 static int keyboard_event(struct notifier_block *nb, unsigned long code, void *param)
 {
     struct keyboard_notifier_param *kp = param;
 
-    if (kp->value == 59 && kp->down) {
+    if (kp->value == KEY_F1 && kp->down) {
+        int keycode = (int)kp->value;
         // Start a timer that will print "Hello"
-        timer_setup(&hello_timer, hello_timer_fn, 0);
-        mod_timer(&hello_timer, jiffies + msecs_to_jiffies(500));
+        // timer_setup(&hello_timer, hello_timer_fn, 0);
+        // mod_timer(&hello_timer, jiffies + msecs_to_jiffies(500));
+        pr_info("Hello %d", keycode);
+
     }
 
     return NOTIFY_OK;
 }
 
+// notifier_call: A function pointer to the callback function that should be called when the notification is triggered.
+// In this case, it's set to keyboard_event, which will run whenever a keyboard event is detected.
+
+// instance of notifier_block structure
 static struct notifier_block keyboard_nb = {
     .notifier_call = keyboard_event
 };
